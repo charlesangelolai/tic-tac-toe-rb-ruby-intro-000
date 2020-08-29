49b29634626e1395d17db2755ccea04836a7fc5f
Charles Lai <charlesangeloplai@gmail.com>
2020-08-29 19:14:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,3 +49,16 @@ def position_taken?(board, index)
     true
   end
 end
+
+# Define turn method
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
