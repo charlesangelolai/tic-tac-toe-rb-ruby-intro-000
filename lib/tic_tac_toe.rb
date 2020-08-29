1a11fc30592053ae9bc511824fd9b3fdd65ab5f4
Charles Lai <charlesangeloplai@gmail.com>
2020-08-29 19:00:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,7 +12,6 @@ WIN_COMBINATIONS = [
 
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
